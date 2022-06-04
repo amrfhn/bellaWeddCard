289c268b1eec5e93a717dcfd0620056e0e9ed425
Amir Farhan bin Jefri <amirfarhan.jfri@gmail.com>
2022-06-05 00:36:41 +0800

    initial commit

---
+++ src/js/global.js
@@ -0,0 +1,43 @@
+import $ from "jquery";
+import ScrollOut from "scroll-out";
+
+$(function () {
+  // console.log($)
+  // $(".animated.text-slide-in").each(function () {
+  //   // const currClientRect = this.getBoundingClientRect();
+  //   // const currLineHeight = parseInt($(this).css('line-height'));
+  //   // const numLines = currClientRect.height / currLineHeight;
+
+  //   $(this).html($(this).text().replace(/\S+/g, "<n>$&</n>"));
+  //   $(this).find("n").attr("data-scroll", true);
+  // });
+
+  // ScrollOut();
+
+  //   console.log(process.env.CLIENT_ID_FB);
+
+});
+
+//fb init sdk
+// (function (d, s, id) {
+//   var js,
+//     fjs = d.getElementsByTagName(s)[0];
+//   if (d.getElementById(id)) {
+//     return;
+//   }
+//   js = d.createElement(s);
+//   js.id = id;
+//   js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v12.0";
+//   fjs.parentNode.insertBefore(js, fjs);
+// })(document, "script", "facebook-jssdk");
+
+// window.fbAsyncInit = function () {
+//   FB.init({
+//     appId: "672101130483714",
+//     cookie: true,
+//     xfbml: true,
+//     version: "v12.0",
+//   });
+
+//   FB.AppEvents.logPageView();
+// };
---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+// Import the main styles
+import "../scss/main.scss";
+
+// Polyfills for Internet Explorer
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+// SVG Polyfill for Internet Explorer
+import svg4everybody from 'svg4everybody';
+svg4everybody()
+
+// Import vendor libraries
+import "bootstrap";
+
+// Global Scripts
+import "./global";
+import "./helpers/validations"
+import "./vue-state"
+
+// Import the JS components
+// import "./components/nav";

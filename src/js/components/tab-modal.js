93b7748454a5d3cd5327a44becbd047564c14c19
Amir Farhan bin Jefri <amirfarhan.jfri@gmail.com>
2022-07-04 13:08:55 +0800

    audio

--- src/js/components/tab-modal.js
+++ src/js/components/tab-modal.js
@@ -6,33 +6,46 @@ $(function () {
     // $(".navbar").show()
   });
 
-  // var userGuestType = window.sessionStorage.getItem("SessionUserGuestType");
-  // if (!userGuestType) {
-  //   openBootstrapPopup();
-  // } else {
-  //   setActiveTab(userGuestType);
-  // }
+  var userGuestType = window.sessionStorage.getItem("SessionUserGuestType");
+  if (!userGuestType) {
+    openBootstrapPopup();
+  } else {
+    setActiveTab(userGuestType);
+  }
+
+  var audioAllowed = window.sessionStorage.getItem("SessionAudioAllowed");
+  if (audioAllowed) {
+    $(".player-button").click();
+  }
 
   $(".button-selectGuestType").click(function () {
     var type = $(this).attr("data-guest");
-    setGuestType(type);
+
+    $(".button-selectGuestType").hide();
+    $(this).show();
+    setSessionStorage(type);
     setActiveTab(type);
-    closeBootstrapPopup();
+    // closeBootstrapPopup();
+  });
+
+  $("#closeStartPopup").click(function () {
+    $(".player-button").click();
   });
 });
 
 function openBootstrapPopup() {
-  $(".navbar").hide();
+  // $(".navbar").hide();
   $("#startpopup-modal").modal("show");
 }
 
 function closeBootstrapPopup() {
   $(".navbar").show();
-  $("#startpopup-modal").modal("hide");
+  // $("#startpopup-modal").modal("hide");
 }
 
-function setGuestType(guestType) {
+function setSessionStorage(guestType) {
   sessionStorage.setItem("SessionUserGuestType", guestType);
+  sessionStorage.setItem("SessionAudioAllowed", true);
 }
 
 function setActiveTab(guestType) {
